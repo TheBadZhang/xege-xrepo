6ef31d39088bc4c4519be168025ccf2844d6c7f3
TheBadZhang <thebadzhang@qq.com>
2024-02-24 21:42:01 +0800

    优化了头文件导出，现在 Mingw 下也可以正常编译了

--- packages/x/xege/xmake.lua
+++ packages/x/xege/xmake.lua
@@ -11,40 +11,53 @@ package("xege")
 
 	add_deps("cmake")
 	add_deps("libpng", "zlib")
-	-- set_sourcedir(".")
+	set_sourcedir(".")
 
 	on_load(function (package)
+
+		package:add("includedirs", "include")  -- 添加头文件搜索路径
+		package:add("linkdirs", "lib")         -- 添加库文件搜索路径
+		package:add("links", "graphics64")     -- 好像不用添加链接库也可以编译通过
+
 		if package:is_plat("windows") then
 			package:config("vs_runtime")
-		end
-	end)
-	on_install(function (package)
-		local configs = {}
-		-- 关闭合并libpng和zlib的选项，使用xmake依赖处理zlib和libpng
-		table.insert(configs, "-DMERGE_LIBPNG_AND_ZLIB=OFF")
-		table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-		table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-		import("package.tools.cmake").install(package, configs)
-		os.cp("src/*.h", package:installdir("include"))
-		os.cp("src/ege", package:installdir("include/ege"))
-
-		package:add("includedirs", "include")
-		package:add("linkdirs", "lib")
-		-- package:add("links", "graphics64")
-
-		-- 不知道为什么，好像mingw编译器导出的选项有问题
-		-- 现在只有 VS 的编译是正常的
-		if package:is_plat("mingw") then
+		elseif package:is_plat("mingw") then
 			local third_lib = {"gdiplus","gdi32","imm32","msimg32","ole32","oleaut32","uuid","winmm"}
 			for _,v in pairs(third_lib) do
-				package:add("links", v)
+				print(v)
+				package:add("syslinks", v)
 			end
+		end
+	end)
+
+	on_install(function (package)
+		-- print("脚本路径", package:scriptdir())
+		-- print(package:cachedir().."\\build_"..package:buildhash():sub(1,8).."\\libgraphics*.a")
+		local configs = {}
+
+		-- 关闭合并libpng和zlib的选项，使用xmake依赖处理zlib和libpng
+		table.insert(configs, "-DMERGE_LIBPNG_AND_ZLIB=OFF")
+		-- table.insert(configs, "-DBUILD_SHARED_LIBS=ON")
+		-- table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+		table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+		import("package.tools.cmake").install(package, configs)
+
+		-- 导出头文件
+		local include_dir = package:installdir("include")                 -- 头文件导出路径
+		local file_or_dir = { "src/ege.h", "src/graphics.h", "src/ege" }  -- 导出的头文件和目录
+		for _,v in ipairs(file_or_dir) do                                 -- 遍历导出的文件和目录
+			os.cp(v, include_dir)
+		end
+
+		-- 导出构建出来的库文件到安装路径中
+		if package:is_plat("mingw") then
 			os.cp(package:cachedir().."\\build_"..package:buildhash():sub(1,8).."\\libgraphics*.a", package:installdir("lib"))
 		end
 	end)
 
-	-- 测试库安装是否正常
 	on_test(function (package)
+
+		-- initgraph无法消歧义，所以这个测试无法使用
 		-- assert(package:has_cxxfuncs("initgraph", {includes = "graphics.h"}))
 		assert(package:check_cxxsnippets({test = [[
 			#include <graphics.h>
